bef5a5ff90e92cafd1af30ce84382b21621a964f
sonilgown <onekwan@smtown.com>
2015-04-17 18:52:21 +0900

    set
    set

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.render('index');
+  res.render('index.html');
 });
+
+router.get('/start', function(req, res, next) {
+  
+  res.render('start.html');
+});
+
 
 module.exports = router;
