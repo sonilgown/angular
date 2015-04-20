07be4b56bae96a097281e6127cf4cbbca56845f3
sonilgown <onekwan@smtown.com>
2015-04-20 14:28:26 +0900

    ewr
    rwq

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
+var path = require('path');
+
+router.use(bodyParser.json());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.render('index.html');
+  res.render('index.html',{title:"dfdasdf"});
 });
 
 router.get('/start', function(req, res, next) {
-  
   res.render('start.html');
+});
+
+router.get('/todo', function(req, res, next) {
+	var jsonObj = require("../public/json/todo.json");
+	res.json(jsonObj);
+  	//res.type('application/json').status(200).json({ error: 'message' });
 });
 
 
